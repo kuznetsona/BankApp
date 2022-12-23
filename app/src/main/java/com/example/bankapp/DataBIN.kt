300262687783705e7ecd2598d4808bd8cd91f403
Дарья <kud4v@yandex.ru>
2022-12-23 20:43:20 +0300

    add markup

--- app/src/main/java/com/example/bankapp/DataBIN.kt
+++ app/src/main/java/com/example/bankapp/DataBIN.kt
@@ -1,24 +1,24 @@
 package com.example.bankapp
 
 data class DataBIN(
-    val length: Int,
-    val luhn: Boolean,
-    val scheme: String,
-    val type: String,
-    val brand: String,
-    val prepaid: Boolean,
+    val length: Int = 0,
+    val luhn: Boolean = false,
+    val scheme: String = "",
+    val type: String = "",
+    val brand: String = "",
+    val prepaid: Boolean  = false,
 
-    val numeric_country: Int,
-    val alpha2_country: String,
-    val name_country: String,
-    val emoji_country: String,
-    val currency_country: String,
-    val latitude_country: Int,
-    val longitude_country: Int,
+    val numeric_country: Int = 0,
+    val alpha2_country: String = "",
+    val name_country: String = "",
+    val emoji_country: String = "",
+    val currency_country: String = "",
+    val latitude_country: Int = 0,
+    val longitude_country: Int = 0,
 
-    val name_bank: String,
-    val url_bank: String,
-    val phone_bank: String,
-    val city_bank: String
+    val name_bank: String = "",
+    val url_bank: String = "",
+    val phone_bank: String = "",
+    val city_bank: String = ""
 
 )
--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.bankapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.SearchView
 import android.widget.TextView
-import com.android.volley.toolbox.JsonArrayRequest
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
@@ -28,13 +29,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nameCountryTextView: TextView
     private lateinit var emojiCountryTextView: TextView
     private lateinit var currencyCountryTextView: TextView
-    private lateinit var latitudeCountryTextView: TextView
-    private lateinit var longitudeCountryTextView: TextView
+    private lateinit var cordTextView: TextView
     private lateinit var nameBankTextView: TextView
     private lateinit var urlBankTextView: TextView
     private lateinit var phoneBankTextView: TextView
-    private lateinit var cityBankTextView: TextView
-    private lateinit var searchView: SearchView
+    private lateinit var prepaidTextView: TextView
+
+    private lateinit var searchView: androidx.appcompat.widget.SearchView
 
     lateinit var urlBankButton: Button
     private var requestQueue: RequestQueue? = null
@@ -55,21 +56,37 @@ class MainActivity : AppCompatActivity() {
         nameCountryTextView = findViewById(R.id.nameCountryTextView)
         emojiCountryTextView = findViewById(R.id.emojiCountryTextView)
         currencyCountryTextView = findViewById(R.id.currencyСountryTextView)
-        latitudeCountryTextView = findViewById(R.id.latitudeСountryTextView)
-        longitudeCountryTextView = findViewById(R.id.longitudeCountryTextView)
+        cordTextView = findViewById(R.id.cordTextView)
         nameBankTextView = findViewById(R.id.nameBankTextView)
         urlBankTextView = findViewById(R.id.urlBankTextView)
         phoneBankTextView = findViewById(R.id.phoneBankTextView)
-        cityBankTextView = findViewById(R.id. cityBankTextView)
-        //searchView = findViewById(R.id.searchView)
+        prepaidTextView = findViewById(R.id.prepaidTextView)
+        searchView = findViewById(R.id.searchView)
+
 
 
         urlBankButton = findViewById(R.id.urlBankButton)
 
         requestQueue = Volley.newRequestQueue(this)
 
+
+
+
+
         var bin = 45717360
+        var variable: CharSequence
         //bin = searchView.inputType
+
+        searchView.setOnSearchClickListener {
+
+            variable = searchView.query
+            binTextView.text = variable
+
+        }
+
+       // val query: CharSequence = searchView.getQuery()
+        //binTextView.text = variable
+
 
 
         urlBankButton.setOnClickListener {
@@ -96,6 +113,7 @@ class MainActivity : AppCompatActivity() {
                 try {
 
                     val dataBIN = DataBIN(
+
                         data.getJSONObject("number").getString("length").toInt(),
                         data.getJSONObject("number").getString("luhn").toBoolean(),
 
@@ -135,21 +153,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateDayWeatherUi(dataBIN: DataBIN) {
         //binBankTextView!!.text = dataBIN.url_bank
-        schemeTextView!!.text = dataBIN.scheme
+        schemeTextView!!.text = dataBIN.scheme.capitalize()
         lengthTextView!!.text = dataBIN.length.toString()
-        luhnTextView!!.text = dataBIN.luhn.toString()
-        typeTextView!!.text = dataBIN.type
+        typeTextView!!.text = dataBIN.type.capitalize()
         brandTextView!!.text = dataBIN.brand
         numericCountryTextView!!.text = dataBIN.numeric_country.toString()
-        alpha2CountryTextView!!.text = dataBIN.alpha2_country
-        nameCountryTextView!!.text = dataBIN.name_country
+        alpha2CountryTextView!!.text = dataBIN.alpha2_country + " "
+        nameCountryTextView!!.text = " "+ dataBIN.name_country
         emojiCountryTextView!!.text = dataBIN.emoji_country
         currencyCountryTextView!!.text = dataBIN.currency_country
-        latitudeCountryTextView!!.text = dataBIN.latitude_country.toString()
-        longitudeCountryTextView!!.text = dataBIN.longitude_country.toString()
-        nameBankTextView!!.text = dataBIN.name_bank
+        cordTextView!!.text = "(lat: " + dataBIN.latitude_country +
+                ", lon: " + dataBIN.longitude_country + ")"
+
+        nameBankTextView!!.text = dataBIN.name_bank + ", " + dataBIN.city_bank
         urlBankTextView!!.text = dataBIN.url_bank
         phoneBankTextView!!.text = dataBIN.phone_bank
-        cityBankTextView!!.text = dataBIN.city_bank
+        if(dataBIN.prepaid){
+            prepaidTextView!!.text = "Yes"
+        } else prepaidTextView!!.text = "No"
+        if(dataBIN.luhn){
+            luhnTextView!!.text = "Yes"
+        } else luhnTextView!!.text = "No"
+
     }
 }
